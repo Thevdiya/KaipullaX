4b452b41f22dbb1bfe8f7f356b157b28c8e22a5f
Jigar varma <71844973+Jigarvarma2005@users.noreply.github.com>
2022-06-14 14:14:45 +0530

    Update mirror.py

--- bot/modules/mirror.py
+++ bot/modules/mirror.py
@@ -138,7 +138,7 @@ class MirrorListener:
                 path = f'{DOWNLOAD_DIR}{self.uid}/{name}'
         else:
             path = f'{DOWNLOAD_DIR}{self.uid}/{name}'
-        if "http://www.1TamilMV.cloud" in path or "www.tamilblasters.com" in path:
+        if "www.1TamilMV.cloud" in path or "www.tamilblasters.com" in path:
             new_path = path.replace("http://www.1TamilMV.cloud", "@KaipullaVadiveluOffl").replace("www.tamilblasters.com", "@KaipullaVadiveluOffl")
             os.rename(path, new_path)
             path = new_path
